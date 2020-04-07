445cba72cebfcd699a84b66d99bcfa32311d574b
Diana-ops <dianaregina22@outlook.com.br>
2020-04-07 18:39:09 -0300

    Atualizando diretorio

--- estudosNodeJS/12-moduloHTTP/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-
-/*Criando um servidor*/
-http.createServer((request, response) => {
-
-	/*Essa mensagem vai aparecer na tela*/
-	response.end('Hello Node')
-})
-.listen(4000, () => console.log("o servidor esta rodando"))
