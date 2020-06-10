dc894a58493e38bf43ea9cae2e45c3c6fcd27ca2
Diana Regina <dianaregina22@outlook.com.br>
2020-06-10 12:59:25 -0300

    Add files via upload

---
+++ code-saga-campuscoders/sql-update/queries/update_estrangeiro.sql
@@ -0,0 +1,5 @@
+-- Escreva abaixo o comando SQL que atualiza Genero para Romance no Livro O
+-- Estrangeiro
+UPDATE Livros 
+SET Genero = "Romance"
+WHERE Titulo = "O Estrangeiro"
---
+++ code-saga-campuscoders/sql-update/queries/update_pernambuco.sql
@@ -0,0 +1,5 @@
+-- Escreva abaixo o comando SQL que corrige Cidade de Pernambuco para Recife
+-- na tabela Pessoas
+UPDATE Pessoas 
+SET Cidade = "Recife"
+WHERE Cidade = "Pernambuco"
