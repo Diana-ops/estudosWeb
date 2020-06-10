8b294bd8c41e83e32dbe71373d898f5b2d6d97a9
Diana Regina <dianaregina22@outlook.com.br>
2020-06-10 12:58:44 -0300

    Add files via upload

---
+++ code-saga-campuscoders/sql-select/queries/query_editora.sql
@@ -0,0 +1,2 @@
+-- Escreva abaixo o comando SQL que busca apenas as editora uma vez.
+SELECT DISTINCT Editora from Livros
---
+++ code-saga-campuscoders/sql-select/queries/query_nome.sql
@@ -0,0 +1,2 @@
+-- Escreva abaixo o comando SQL que busca apenas os nomes das pessoas.
+SELECT Nome from Pessoas
